a19890e520440bf1cb3787b6a3938fd0abe3f1f8
Aditya Kumar Sinha <adityauv11@gmail.com>
2025-08-16 00:54:36 +0530

    model architecture

--- model.py
+++ model.py
@@ -8,21 +8,21 @@ class ResidualBlock(nn.Module):
                                3, stride, padding = 1, bias = False)
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.conv2 = nn.Conv2d(out_channels, out_channels,
-                               3, stride, padding = 1, bias = False)
+                               3, padding = 1, bias = False)
         self.bn2 = nn.BatchNorm2d(out_channels)
 
         self.shortcut = nn.Sequential()
         self.use_shortcut = stride != 1 or in_channels != out_channels
         if self.use_shortcut:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, 1, stride - stride, bias=False), nn.BatchNorm2d(out_channels)
+                nn.Conv2d(in_channels, out_channels, 1, stride=stride, bias=False), nn.BatchNorm2d(out_channels)
             )
 
     def forward(self, x):
         out = self.conv1(x)
         out = self.bn1(out)
         out = torch.relu(out)
-        out = self.conv2(x)
+        out = self.conv2(out)
         out = self.bn2(out)
         shortcut = self.shortcut(x) if self.use_shortcut else x
         out_add = out + shortcut
@@ -32,30 +32,24 @@ class ResidualBlock(nn.Module):
 
 
 class AudioCNN(nn.Module):
-    def __init__(self, num_classes = 50):
+    def __init__(self, num_classes=50):
         super().__init__()
-        self.Conv1 = nn.Sequential(
-            nn.Conv2d(1, 64, 7, stride=2, padding=3, bias=False), nn.BatchNorm2d(64), nn.ReLU(inplace=True), nn.MaxPool2d(3, stride=2, padding=1)
-        )
-        self.layer1 = nn.ModuleList(
-            [ResidualBlock(64, 64) for i in range(3)]
-        )
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 64, 7, stride=2, padding=3, bias=False), nn.BatchNorm2d(64), nn.ReLU(inplace=True), nn.MaxPool2d(3, stride=2, padding=1))
+        self.layer1 = nn.ModuleList([ResidualBlock(64, 64) for i in range(3)])
         self.layer2 = nn.ModuleList(
-            [ResidualBlock(64 if i==0 else 128, 128) for i in range(4)]
-        )
+            [ResidualBlock(64 if i == 0 else 128, 128, stride=2 if i == 0 else 1) for i in range(4)])
         self.layer3 = nn.ModuleList(
-            [ResidualBlock(128 if i==0 else 256, 256) for i in range(6)]
-        )
+            [ResidualBlock(128 if i == 0 else 256, 256, stride=2 if i == 0 else 1) for i in range(6)])
         self.layer4 = nn.ModuleList(
-            [ResidualBlock(256 if i==0 else 512, 512) for i in range(3)]
-        )
+            [ResidualBlock(256 if i == 0 else 512, 512, stride=2 if i == 0 else 1) for i in range(3)])
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.dropout = nn.Dropout(0.5)
         self.fc = nn.Linear(512, num_classes)
 
     def forward(self, x):
-        x = self.Conv1(x)
+        x = self.conv1(x)
         for block in self.layer1:
             x = block(x)
         for block in self.layer2:
